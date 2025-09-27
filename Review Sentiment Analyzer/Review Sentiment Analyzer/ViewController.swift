4565a7df0b6e4de72a7d65f92ba57b63e1d44373
rex <hi.chuntachen@gmail.com>
2025-09-27 14:38:53 +0800

    add review ml

--- Review Sentiment Analyzer/Review Sentiment Analyzer/ViewController.swift
+++ Review Sentiment Analyzer/Review Sentiment Analyzer/ViewController.swift
@@ -1,37 +1,57 @@
-//
-//  ViewController.swift
-//  Review Sentiment Analyzer
-//
-//  Created by Nyisztor, Karoly on 10/3/18.
-//  Copyright © 2018 Nyisztor, Karoly. All rights reserved.
-//
-// **Machine Learning with Core ML 2 and Swift** - Source Code
-//
-// Get the course **[on Udemy using this discounted coupon](https://www.udemy.com/machine-learning-with-core-ml2-and-swift/?couponCode=GITHUB)**
-//
-// The book version is available **[on Amazon](https://www.amazon.com/dp/B07F2NYDTH)**
-//
-// _ _ _
-//: ## Review sentiment analyzer app
-//: This app uses the review sentiment analyzer to determine the sentiment of a written review
-//:
-//: - Callout(Interested in Swift programming?):
-//: Check out my **[Youtube channel](https://www.youtube.com/c/swiftprogrammingtutorials)**
-//: my courses on **[Udemy](https://www.udemy.com/user/karolynyisztor/)**, **[Lynda](https://www.lynda.com/Karoly-Nyisztor/9655357-1.html)** and **[Pluralsight](https://www.pluralsight.com/profile/author/karoly-nyisztor)**.
-//: Website **[www.leakka.com](http://www.leakka.com)**
-//: ---
-
 import UIKit
 import NaturalLanguage
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
     
-    @IBOutlet weak var sentimentImageView: UIImageView!
-    @IBOutlet weak var textView: UITextView!
-    @IBOutlet weak var clearButton: UIButton!
-    
+    @IBOutlet var textView: UITextView!
+    @IBOutlet var clearButton: UIButton!
+    @IBOutlet var statusText: UILabel!
+        
+    private lazy var sentimentClassifier: NLModel? = {
+        let model = try? NLModel (mlModel: ReviewClassifier().model)
+        return model
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textView.delegate = self
+        toggleClearButtonEnable()
+    }
+    
+    @IBAction func onClearTest(_ sender: Any)
+    {
+        textView.text = ""
+        setStatusText("--")
+    }
+    
+    // UITextViewDelegate
+    func textViewDidChange(_ textView: UITextView) {
+        toggleClearButtonEnable()
+        setStatusText("--")
+        if textView.text.count > 0 {
+            if let label = sentimentClassifier?.predictedLabel(for: textView.text) as? String {
+                print("lable \(label)")
+                switch label {
+                case "positive":
+                    setStatusText(":)")
+                    break
+                case "negative":
+                    setStatusText(":(")
+                    break
+                default:
+                    setStatusText("--")
+                    break
+                }
+            }
+        }
+    }
+    
+    private func setStatusText(_ text: String)
+    {
+        statusText.text = text
+    }
+    
+    private func toggleClearButtonEnable()
+    {
+     //   clearButton.isEnabled = !textView.text.isEmpty
     }
 }
